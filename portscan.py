2e0e2a7bb804d8907feef423ace20755094dedce
Godoy <107765540+Godoy-png@users.noreply.github.com>
2023-01-21 15:46:23 -0300

    Add files via upload

---
+++ portscan.py
@@ -0,0 +1,13 @@
+import socket
+
+ports = [21,22,80,443,445,3306,25]
+
+for port in ports:
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	client.settimeout(0.1)
+	code = client.connect_ex(("website.com", port))
+	if code == 0:
+		print(port, "PORT OPEN")
+
+
+#discord for help:  G.#4179
